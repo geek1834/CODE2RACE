6887cce5ead581d9beba6e4992b227dd1a71c649
geek1834 <44375459+geek1834@users.noreply.github.com>
2018-10-22 23:33:10 +0530

    comparelist.py

---
+++ SOLUTIONS/comparelist1.py
@@ -0,0 +1,9 @@
+a = [1, 2, 3, 4, 5]
+b = [9, 8, 7, 6, 5]
+
+lista =set(a)
+listb =set(b)   
+print listb.intersection(lista)   
+returnMatches = set(['5']) #output 
+
+print " ".join(str(return) for return in returnMatches )
